0e8cc3a31690edb696b6598b05a404751b3eaf3f
chun-fu chen <iamcjf@gmail.com>
2024-07-29 23:15:29 +0800

    1. Add Google Oauth

--- go.mod
+++ go.mod
@@ -3,12 +3,14 @@ module asset-go
 go 1.22.0
 
 require (
+	cloud.google.com/go/compute/metadata v0.3.0 // indirect
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/bytedance/sonic v1.11.9 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.4 // indirect
+	github.com/gin-contrib/sessions v1.0.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.10.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -16,6 +18,9 @@ require (
 	github.com/go-playground/validator/v10 v10.22.0 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
+	github.com/gorilla/context v1.1.2 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/gorilla/sessions v1.2.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.8 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -28,6 +33,7 @@ require (
 	golang.org/x/arch v0.8.0 // indirect
 	golang.org/x/crypto v0.25.0 // indirect
 	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/oauth2 v0.21.0 // indirect
 	golang.org/x/sys v0.22.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
--- src/main.go
+++ src/main.go
@@ -1,23 +1,70 @@
 package main
 
 import (
+	dbmodel "asset-go/src/models"
+	"context"
+	"errors"
 	"log"
+	"net/http"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
-	"asset-go/src/models"
 	_ "github.com/go-sql-driver/mysql"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
+var conf = &oauth2.Config{
+	ClientID:     "<ClientID>",
+	ClientSecret: "<ClientSecret>",
+	RedirectURL:  "http://127.0.0.1:3000/oauth_callback",
+	Scopes:       []string{"profile", "email"},
+	Endpoint:     google.Endpoint,
+}
 
+func CheckToken(c *gin.Context) {
+	session := sessions.Default(c)
+	token := session.Get("token")
+	if token == nil {
+		log.Println("Session token is empty.")
+		url := conf.AuthCodeURL("state")
+		c.Redirect(http.StatusFound, url)
+	}
+	log.Println("Session ID:", token)
+}
 
 func main() {
-	// Hello world, the web server
 
 	r := gin.Default()
-	
+	store := cookie.NewStore([]byte("secret"))
+	r.Use(sessions.Sessions("mysession", store))
+
+	r.GET("/oauth_callback", func(c *gin.Context) {
+		state := c.Query("state")
+		if state != "state" {
+			c.AbortWithError(http.StatusUnauthorized, errors.New("invalid csrf token"))
+			return
+		}
+		code := c.Query("code")
+		var err error
+		token, err := conf.Exchange(context.Background(), code)
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		log.Println("Google Oauth token", token)
+
+		session := sessions.Default(c)
+		session.Set("token", "pass")
+		session.Save()
+		c.Redirect(http.StatusFound, "/assets")
+	})
+	r.Use(CheckToken)
 	r.GET("/assets", func(c *gin.Context) {
 		assets := dbmodel.ListMyAsset()
 		log.Println("assets", assets)
-		
+
 		c.JSON(200, gin.H{
 			"assets": assets,
 		})
@@ -26,7 +73,7 @@ func main() {
 	r.POST("/asset", func(c *gin.Context) {
 		reqAsset := new(dbmodel.Asset)
 		err := c.Bind(reqAsset)
-		if err != nil{
+		if err != nil {
 			log.Fatalln("Failed to bind request payload.", err)
 		}
 		aid := dbmodel.AddAsset(*reqAsset)
@@ -38,7 +85,7 @@ func main() {
 		aid := c.Param("aid")
 		reqAsset := new(dbmodel.Asset)
 		err := c.Bind(reqAsset)
-		if err != nil{
+		if err != nil {
 			log.Fatalln("Failed to bind request payload.", err)
 		}
 		dbmodel.UpdateAsset(aid, *reqAsset)
@@ -53,6 +100,6 @@ func main() {
 			"deletedAsset": aid,
 		})
 	})
-	
+
 	r.Run()
 }
